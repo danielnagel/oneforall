6b69e540d95d56fb9fef39d352f6cf339e393ae2
Daniel <daniel.nagel@propersonal.de>
2018-02-15 13:15:53 +0100

    SharePoint Handler refactored. Alles in Funktionen ausgelagert.

--- sharepointhandler/sharepointhandler.py
+++ sharepointhandler/sharepointhandler.py
@@ -1,80 +1,158 @@
 import requests
-from requests_ntlm import HttpNtlmAuth
-#  import json #print(json.dumps(result.json(), sort_keys=True, indent=4))
-#  Beautified eine JSON-Datei
-from os.path import join, dirname
-from getpass import getpass
 import re
 import json
-
-username = 'propersonal\\'
-username += input("Benutzernamen eingeben: ")
-password = getpass("Passwort eingeben: ")
-fileName = "test.txt"
-
-#  SharePoint URL und Ziel Bibliothek
-sharePointUrl = 'https://sharepoint.propersonal.de'
-folderUrl = '/BZ-Rechner/'
-#  Request URL vorbereiten
-requestUrl = sharePointUrl + folderUrl
-serverRelativeUrl = 'SiteAssets/BZ-Rechner'
-getFiles = "_api/web/GetFolderByServerRelativeUrl('" + serverRelativeUrl + "')/files"
+from requests_ntlm import HttpNtlmAuth
+from os.path import join, exists
 
 
-#  Header für die Kommunikation mit SharePoint vorbereiten
-headers = {'Content-Type': 'application/json; odata=verbose',
-           'accept': 'application/json;odata=verbose'}
-
-#  Request ausführen, um das FormDigestValue zu erhalten.
-#  Dieser wird gebraucht, um unseren upload zu authentifizieren.
-result = requests.post(requestUrl + '_api/contextinfo', headers = headers,
-                  auth = HttpNtlmAuth(username, password))
-
-print("---")
-print("Ergebnis FormDigestValue holen:", result.status_code)
-print("---")
-
-formDigestValue = result.json()['d']['GetContextWebInformation']['FormDigestValue']
-
-#  Datei lesen
-file = open(join(dirname(__file__), fileName), 'rb')
-#  os.path.dirname(__file__) gibt den Pfad zum Python Skript aus
-
-#  Header um den erhaltenen FormDigestValue aktualisieren.
-headers = {'Content-Type': 'application/json; odata=verbose',
-           'accept': 'application/json;odata=verbose',
-           'x-requestdigest': formDigestValue}
-
-#  Datei hochladen
-result = requests.post(requestUrl + getFiles +
-                       "/add(overwrite=true, url='test.txt')",
-                       headers = headers,
-                       auth = HttpNtlmAuth(username, password),
-                       data=file.read())
-
-print("---")
-print("Ergebnis Datei hochladen:", result.status_code)
-print("---")
-
-#  Dateien ausgeben
-result = requests.post(requestUrl + getFiles, headers = headers,
-                       auth = HttpNtlmAuth(username, password))
-
-print("---")
-print("Ergebnis Datei ausgeben:", result.status_code)
-print("---")
-
-check = re.compile('4..')
-if check.match(str(result.status_code)) != None:
+def isJSON(string):
+    """ Prüft ob eine valide JSON vorliegt.
+        :param string: Die zu validierende JSON.
+        :return: True bei einer validen JSON, ansonsten False.
+    """
     try:
-        errMessage = result.json()['error']['message']['value']
-        print(errMessage)
-    except json.decoder.JSONDecodeError:
-        print("Keine valide JSON-Datei!")
-        print("Die Datei konnte nicht hochgeladen werden.")
-else:
-    print(result.status_code)
+        json.loads(string)
+        return True
+    except ValueError:
+        return False
 
-    files = result.json()['d']['results']
-    for file in files:
-        print(file['Name'])
+
+def isBadRequest(status_code):
+    """ Prüft ob ein 4xx HTTP Status Code vorliegt.
+        :param status_code: Der zu prüfende HTTP Status Code.
+        :return: True wenn ein 4xx HTTP Status Code vorliegt, ansonsten False.
+    """
+    check = re.compile('4..')
+    if check.match(str(status_code)) is not None:
+        return True
+    else:
+        return False
+
+
+def printReadableJSON(json):
+    """ Gibt eine JSON leserlich aus, für Debug Zwecke.
+        :param json: Die zu verschönernde JSON.
+    """
+    print(json.dumps(result.json(), sort_keys=True, indent=4))
+
+
+def getDigestValue(username, password, requestUrl):
+    """ Der Digest Value hat die Funktionsweise eines Cookies.
+        Dieser muss bei jeder Anfrage mitgesendet werden, um sich zu
+        authentifizieren.
+        :param username: Der Benutzername.
+        :param password: Das Benutzer Passwort.
+        :param requestUrl: Die URL zur SharePoint Webseite bzw. Unterwebseite.
+        :return: Digest Value, welcher den Benutzer authentifiziert. Oder None
+                 wenn der request fehlgeschlagen ist.
+    """
+    #  Header für die Kommunikation mit SharePoint vorbereiten
+    headers = {'Content-Type': 'application/json; odata=verbose',
+               'accept': 'application/json;odata=verbose'}
+
+    #  Request ausführen, um den Digest Value zu erhalten.
+    result = requests.post(requestUrl + '_api/contextinfo', headers = headers,
+                      auth = HttpNtlmAuth(username, password))
+
+    #  Request Status Code überprüfen
+    if isBadRequest(result.status_code):
+        print("Anfrage fehlgeschlagen.")
+        print("Keinen Digest Value erhalten.")
+        return None
+
+    # Digest Value aus der JSON auslesen
+    if isJSON(result.content):
+        digestValue = result.json()['d']['GetContextWebInformation']['FormDigestValue']
+        return digestValue
+    else:
+        print("Keine valide JSON-Datei!")
+        print("Keinen Digest Value erhalten.")
+        return None
+
+
+def uploadFile(username, password, path, fileName, requestUrl,
+               serverRelativeUrl, digestValue):
+    """ Lädt eine Datei ins SharePoint hoch.
+        :param username: Der Benutzername.
+        :param password: Das Benutzer Passwort.
+        :param path: Pfad zur Datei die hochgeladen werden soll.
+        :param fileName: Name der Datei die hochgeladen werden soll.
+        :param requestUrl: Die URL zur SharePoint Webseite bzw. Unterwebseite.
+        :param serverRelativeUrl: Relativer Serverpfad welcher das Upload
+                                  Verzeichnis spezifiziert.
+        :param digestValue: Der Digest Value authentifiziert den Benutzer.
+        :return: True wenn alles geklappt hat, ansonsten False.
+    """
+    #  Header zum hochladen der Datei vorbereiten.
+    headers = {'Content-Type': 'application/json; odata=verbose',
+               'accept': 'application/json;odata=verbose',
+               'x-requestdigest': digestValue}
+
+    # Relativer Pfad zum SharePoint Verzeichnis
+    getFiles = "_api/web/GetFolderByServerRelativeUrl('" + serverRelativeUrl \
+            + "')/files"
+
+    #  Datei lesen
+    uploadFile = join(path, fileName)
+    if exists(uploadFile):
+        try:
+            file = open(uploadFile, 'rb')
+        except:
+            print("Datei '" + uploadFile + "' kann nicht geöffnet werden.")
+            return False
+    else:
+        print("Die Datei '" + uploadFile + "' ist nicht vorhanden!")
+        return False
+
+
+    #  Datei hochladen
+    result = requests.post(requestUrl + getFiles +
+                           "/add(overwrite=true, url='" + fileName + "')",
+                           headers = headers, data=file.read(),
+                           auth = HttpNtlmAuth(username, password))
+
+    #  Request Status Code überprüfen
+    if isBadRequest(result.status_code):
+        print("Anfrage fehlgeschlagen.")
+        print("Keine Datei hochgeladen.")
+        return False
+    else:
+        print("Datei erfolgreich hochgeladen.")
+        return True
+
+
+def printFiles(username, password, requestUrl, serverRelativeUrl, digestValue):
+    """ Listet alle Dateien in einem SharePoint Verzeichnis auf.
+        :param username: Der Benutzername.
+        :param password: Das Benutzer Passwort.
+        :param requestUrl: Die URL zur SharePoint Webseite bzw. Unterwebseite.
+        :param serverRelativeUrl: Relativer Serverpfad welcher das Upload
+                                  Verzeichnis spezifiziert.
+        :param digestValue: Der Digest Value authentifiziert den Benutzer.
+    """
+    #  Header zum auflisten der Dateien vorbereiten.
+    headers = {'Content-Type': 'application/json; odata=verbose',
+               'accept': 'application/json;odata=verbose',
+               'x-requestdigest': digestValue}
+
+    # Relativer Pfad zum SharePoint Verzeichnis
+    getFiles = "_api/web/GetFolderByServerRelativeUrl('" + serverRelativeUrl \
+        + "')/files"
+
+    #  Dateien ausgeben
+    result = requests.post(requestUrl + getFiles, headers = headers,
+                           auth = HttpNtlmAuth(username, password))
+
+    #  Request Status Code überprüfen
+    if isBadRequest(result.status_code):
+        print("Anfrage fehlgeschlagen.")
+        print("Dateien können nicht ausgeben werden.")
+
+    # Digest Value aus der JSON auslesen
+    if isJSON(result.content):
+        files = result.json()['d']['results']
+        for file in files:
+            print(file['Name'])
+    else:
+        print("Keine valide JSON-Datei!")
+        print("Dateien können nicht ausgeben werden.")
